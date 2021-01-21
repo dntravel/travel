3ccee7cb06d8380501922fd0351e41dbf251b8f3
maq-97 <asad.muhammad.qazi@gmail.com>
2021-01-21 23:46:33 +0500

    creating new file for js

---
+++ home-2.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function () {
+    checkVisit();
+})
+function checkVisit() {
+    if(localStorage.getItem('visited')){
+        window.location.replace("https://celine-76c1f9-6f513b5b480-082708aff2d37.webflow.io/");
+    }else{
+        localStorage.setItem('visited', 'true');
+    }
+}
