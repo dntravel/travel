f792a373188e5ba9a7d7d8e7fa8fc2bfc90dc0da
Benjamin Gambling <benjamin.gambling@gmail.com>
2021-01-23 18:04:24 -0800

    wistia player silentAutoPlay after 2secs

--- gallery.js
+++ gallery.js
@@ -1,3 +1,23 @@
+setTimeout(() => {
+  document.getElementById("wistia").innerHTML = `<div
+              class="wistia_responsive_wrapper"
+              style="
+                height: 100%;
+                left: 0;
+                position: absolute;
+                top: 0;
+                width: 100%;
+              "
+            >
+              <div
+                class="wistia_embed wistia_async_wwmuxeowoj seo=false videoFoam=true silentAutoPlay=true"
+                style="height: 100%; position: relative; width: 100%"
+              >
+                &nbsp;
+              </div>
+            </div>`;
+}, 2000);
+
 async function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
